166c2c6469c51b3b12f5a944e75a9327e9696763
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2024-08-01 12:22:39 -0300

    Cambie diseño de cards de asistencia (tamaños)

--- src/pages/alumnos/Asistencias.jsx
+++ src/pages/alumnos/Asistencias.jsx
@@ -8,22 +8,22 @@ const Asistencias = () => {
       <h4 className="text-center my-2 titulo">Asistencias</h4>
       <Row className="row-asistencias d-flex justify-content-center">
         <Col xs={12} md={6} lg={5}>
-          <article className="mb-3 p-3 p-lg-1 asistencia-card text-center">
-            <p className="asistencia-titulo">Total días de clases</p>
-            <p className="asistencia-valor">150</p>
+          <article className="mb-3 p-3 p-lg-2 asistencia-card text-center">
+            <p className=" text-light my-2 my-lg-1">Total días de clases</p>
+            <p className="fs-1 text-light my-2 my-lg-1">150</p>
           </article>
-          <article className="mb-3 p-3 p-lg-1 asistencia-card text-center">
-            <p className="asistencia-titulo">Total inasistencias</p>
-            <p className="asistencia-valor">10</p>
+          <article className="mb-3 p-3 p-lg-2 asistencia-card text-center">
+            <p className=" text-light my-2 my-lg-1">Total inasistencias</p>
+            <p className="fs-1 text-light my-2 my-lg-1">10</p>
           </article>
-          <article className="mb-3 p-3 p-lg-1 asistencia-card text-center">
-            <p className="asistencia-titulo">Inasistencias justificadas</p>
-            <p className="asistencia-valor">2</p>
+          <article className="mb-3 p-3 p-lg-2 asistencia-card text-center">
+            <p className=" text-light my-2 my-lg-1">Inasistencias justificadas</p>
+            <p className="fs-1 text-light my-2 my-lg-1">2</p>
           </article>
         </Col>
         <Col xs={12} md={6} lg={5}>
           <div className="d-flex justify-content-center ">
-            <article className="mb-3 p-3 p-lg-1  asistencia-card  inasistencias-fechas text-center">
+            <article className="mb-3 p-3 p-lg-2  asistencia-card  inasistencias-fechas text-center">
               <p className="fechas-titulo">Fechas inasistencias:</p>
               <ol>
                 <li>26/07/2024</li>
