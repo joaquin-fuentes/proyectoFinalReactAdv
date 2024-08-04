17ee93b25f67832091c0b31615abab9916029759
Andres Santamarina <andresesantamarina@gmail.com>
2024-08-04 03:58:00 -0300

    maquetado para las asistencias del docente. Titulo en mayusculas y un my-4 porque en sm se veia muy junto

--- src/pages/docentes/AsistenciasDocente.jsx
+++ src/pages/docentes/AsistenciasDocente.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import { Col, Container, Row } from "react-bootstrap";
+import "./Docentes.css";
 
 const AsistenciasDocente = () => {
-    return (
-        <div>
-            asistencia docente
-        </div>
-    );
+  return (
+    <Container className="asistencias-container pt-md-3">
+      <h4 className="text-center my-2 titulo my-4">ASISTENCIAS</h4>
+      <Row className="row-asistencias d-flex justify-content-center">
+        <Col xs={12} md={6} lg={5}>
+          <article className="mb-3 p-3 p-lg-2 asistencia-card text-center">
+            <p className=" text-light my-2 my-lg-1">Total días de clases</p>
+            <p className="fs-1 text-light my-2 my-lg-1">150</p>
+          </article>
+          <article className="mb-3 p-3 p-lg-2 asistencia-card text-center">
+            <p className=" text-light my-2 my-lg-1">Total inasistencias</p>
+            <p className="fs-1 text-light my-2 my-lg-1">10</p>
+          </article>
+          <article className="mb-3 p-3 p-lg-2 asistencia-card text-center">
+            <p className=" text-light my-2 my-lg-1">
+              Inasistencias justificadas
+            </p>
+            <p className="fs-1 text-light my-2 my-lg-1">2</p>
+          </article>
+        </Col>
+        <Col xs={12} md={6} lg={5}>
+          <div className="d-flex justify-content-center ">
+            <article className="mb-3 p-3 p-lg-2  asistencia-card  inasistencias-fechas text-center">
+              <p className="fechas-titulo">Fechas inasistencias:</p>
+              <ol>
+                <li>26/07/2024</li>
+                <li>26/07/2024</li>
+                <li>26/07/2024 (justificada)</li>
+                <li>26/07/2024 (justificada)</li>
+                <li>26/07/2024</li>
+                <li>26/07/2024</li>
+                <li>26/07/2024</li>
+                <li>26/07/2024</li>
+                <li>26/07/2024</li>
+                <li>26/07/2024</li>
+              </ol>
+            </article>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default AsistenciasDocente;
