55b7c44abc20f808a6bed279d50fe8a5d2617885
Andres Santamarina <andresesantamarina@gmail.com>
2024-08-04 04:30:30 -0300

    maquetado del listado de las materias, le puse my-5 mb-3 al titulo. En la tabla, el btn te lleva al listado de alumnos

--- src/pages/docentes/ListadoMaterias.jsx
+++ src/pages/docentes/ListadoMaterias.jsx
@@ -1,11 +1,95 @@
-import React from 'react';
+import { Container, Form, Table } from "react-bootstrap";
+import "./Docentes.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const ListadoMaterias = () => {
-    return (
-        <div>
-            materias docente
-        </div>
-    );
+  return (
+    <Container className="text-center px-md-5 py-md-2">
+      <h4 className="mt-5 mb-3 titulo">MIS MATERIAS</h4>
+
+      <Form.Group className="d-flex align-items-center justify-content-center w-md-50 ms-3">
+        <Form.Label className="m-0 p-2">Buscar Materia:</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Nombre materia"
+          className="w-50"
+        />
+      </Form.Group>
+      <Table striped hover responsive className="mt-3 rounded">
+        <thead>
+          <tr className="">
+            <th className="tableMaterias fw-bold">Materia</th>
+            <th className="tableMaterias fw-bold">Año</th>
+            <th className="tableMaterias fw-bold">División</th>
+            <th className="tableMaterias fw-bold">Horario</th>
+            <th className="tableMaterias fw-bold">Alumnos</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="tableMaterias">Matemáticas</td>
+            <td className="tableMaterias">2°</td>
+            <td className="tableMaterias">B</td>
+            <td className="tableMaterias">Lunes 10:00 a 12:00</td>
+            <td className="tableMaterias">
+              Ver listado{" "}
+              <button className="btn btn-outline-light">
+                <i className="bi bi-box-arrow-up-right"></i>
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td className="tableMaterias">Matemáticas</td>
+            <td className="tableMaterias">2°</td>
+            <td className="tableMaterias">B</td>
+            <td className="tableMaterias">Lunes 10:00 a 12:00</td>
+            <td className="tableMaterias">
+              Ver listado{" "}
+              <button className="btn btn-outline-light">
+                <i className="bi bi-box-arrow-up-right"></i>
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td className="tableMaterias">Matemáticas</td>
+            <td className="tableMaterias">2°</td>
+            <td className="tableMaterias">B</td>
+            <td className="tableMaterias">Lunes 10:00 a 12:00</td>
+            <td className="tableMaterias">
+              Ver listado{" "}
+              <button className="btn btn-outline-light">
+                <i className="bi bi-box-arrow-up-right"></i>
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td className="tableMaterias">Matemáticas</td>
+            <td className="tableMaterias">2°</td>
+            <td className="tableMaterias">B</td>
+            <td className="tableMaterias">Lunes 10:00 a 12:00</td>
+            <td className="tableMaterias">
+              Ver listado{" "}
+              <button className="btn btn-outline-light">
+                <i className="bi bi-box-arrow-up-right"></i>
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td className="tableMaterias">Matemáticas</td>
+            <td className="tableMaterias">2°</td>
+            <td className="tableMaterias">B</td>
+            <td className="tableMaterias">Lunes 10:00 a 12:00</td>
+            <td className="tableMaterias">
+              Ver listado{" "}
+              <button className="btn btn-outline-light">
+                <i className="bi bi-box-arrow-up-right"></i>
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+    </Container>
+  );
 };
 
 export default ListadoMaterias;
