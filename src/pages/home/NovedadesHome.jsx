03de100474a2c52bf3ee0fca18b60ab4d702c6ad
Alejo Herrera <alejoherrera1166@gmail.com>
2024-08-13 12:47:16 -0300

    Read de novedades funcionando en la page de alumno. Con lógica de filtro.

--- src/pages/home/NovedadesHome.jsx
+++ src/pages/home/NovedadesHome.jsx
@@ -1,12 +1,56 @@
 import { Col, Container, Row } from "react-bootstrap";
 import "../docentes/Docentes.css";
+import useNovedadesStore from "../../stores/Novedades-Store.jsx";
+import { useEffect } from "react";
 
 const NovedadesHome = () => {
+  const novedades = useNovedadesStore((state) => state.novedades);
+  const loading = useNovedadesStore((state) => state.loading);
+  const error = useNovedadesStore((state) => state.error);
+  const getNovedades = useNovedadesStore((state) => state.getNovedades);
+
+  useEffect(() => {
+    getNovedades();
+  }, [getNovedades]);
+
+  if (loading) {
+    return (
+      <section className="vh-100 d-flex flex-column justify-content-center">
+        <p className="text-light">Cargando novedades..</p>
+      </section>
+    );
+  }
+
+  if (error) {
+    return (
+      <section className="vh-100 d-flex flex-column justify-content-center">
+        <p className="text-light">Error: {error}</p>
+      </section>
+    );
+  }
+  const novedadesHome = novedades.filter(
+    (novedad) => novedad.destinatario === "todos"
+  );
+
   return (
     <Container className="p-md-4 fuenteNovedades">
       <h4 className="my-5 text-center titulo">NOVEDADES</h4>
       <Row className="d-flex justify-content-center">
-        <Col md={6} lg={6}>
+        {novedadesHome.map((novedad) => (
+          <Col md={6} lg={4} key={novedad.id}>
+            <article className="p-3 my-2 mx-1 bg-azulOscuro cardNovedad rounded text-light">
+              <h6 className="text-center">{novedad.titulo}</h6>
+              <div className="mt-4 mb-1">
+                <img
+                  className="imgNovedad"
+                  src={novedad.url_img}
+                  alt={"imagen de " + novedad.titulo}
+                />
+              </div>
+            </article>
+          </Col>
+        ))}
+        {/* <Col md={6} lg={6}>
           <article className="p-3 my-2 mx-1 bg-azulOscuro cardNovedad rounded text-light">
             <h6 className="text-center">Fechas de actos patrios</h6>
             <div className="mt-4 mb-1">
@@ -77,7 +121,7 @@ const NovedadesHome = () => {
               />
             </div>
           </article>
-        </Col>
+        </Col> */}
       </Row>
     </Container>
   );
