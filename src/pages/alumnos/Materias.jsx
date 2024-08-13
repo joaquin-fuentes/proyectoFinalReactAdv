22edfe63e3c303cc689fe50e7205ff26c47ab53e
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2024-08-13 17:51:18 -0300

    funcionalidad para que se muestren las materias que cursa cada alumno

--- src/pages/alumnos/Materias.jsx
+++ src/pages/alumnos/Materias.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Form, Table } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import "./Alumnos.css";
+import useAuth from "../../stores/Auth-Store";
+import useCursosStore from "../../stores/Cursos-Store";
 
 const Materias = () => {
+  const { user } = useAuth();
+  const { cursos, obtenerCursos } = useCursosStore();
+  const [materias, setMaterias] = useState([]);
+
+  useEffect(() => {
+    const fetchCursos = async () => {
+      await obtenerCursos();
+      const cursosFiltrados = cursos.filter((curso) =>
+        curso.alumnos.some((alumno) => alumno.alumnoID === user.id)
+      );
+      const materiasFiltradas = cursosFiltrados.flatMap(
+        (curso) => curso.materias
+      );
+      setMaterias(materiasFiltradas);
+    };
+
+    fetchCursos();
+  }, [obtenerCursos, cursos, user.id]);
+
   return (
     <Container className="text-center px-md-5 py-md-2">
       <h4 className="my-3 titulo">Listado de materias en curso</h4>
@@ -16,14 +35,10 @@ const Materias = () => {
           className="w-50"
         />
       </Form.Group>
-      <Table
-        striped
-        hover
-        responsive
-        className="mt-3 rounded"
-      >
+
+      <Table striped hover responsive className="mt-3 rounded">
         <thead>
-          <tr className="">
+          <tr>
             <th className="tableMaterias fw-bold">Materia</th>
             <th className="tableMaterias fw-bold">Año</th>
             <th className="tableMaterias fw-bold">Docente</th>
@@ -35,132 +50,41 @@ const Materias = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td className="tableMaterias">Matemáticas</td>
-            <td className="tableMaterias">2°B</td>
-            <td className="tableMaterias">Prof. Juan Perez</td>
-            <td className="tableMaterias">6</td>
-            <td className="tableMaterias">9</td>
-            <td className="tableMaterias">7</td>
-            <td className="tableMaterias">7.3</td>
-            <td className="tableMaterias text-success">Aprobado</td>
-          </tr>
-          <tr>
-            <td className="tableMaterias">Matemáticas</td>
-            <td className="tableMaterias">2°B</td>
-            <td className="tableMaterias">Prof. Juan Perez</td>
-            <td className="tableMaterias">6</td>
-            <td className="tableMaterias">9</td>
-            <td className="tableMaterias">7</td>
-            <td className="tableMaterias">7.3</td>
-            <td className="tableMaterias text-success">Aprobado</td>
-          </tr>
-          <tr>
-            <td className="tableMaterias">Matemáticas</td>
-            <td className="tableMaterias">2°B</td>
-            <td className="tableMaterias">Prof. Juan Perez</td>
-            <td className="tableMaterias">6</td>
-            <td className="tableMaterias">9</td>
-            <td className="tableMaterias">7</td>
-            <td className="tableMaterias">7.3</td>
-            <td className="tableMaterias text-success">Aprobado</td>
-          </tr>
-          <tr>
-            <td className="tableMaterias">Matemáticas</td>
-            <td className="tableMaterias">2°B</td>
-            <td className="tableMaterias">Prof. Juan Perez</td>
-            <td className="tableMaterias">6</td>
-            <td className="tableMaterias">9</td>
-            <td className="tableMaterias">7</td>
-            <td className="tableMaterias">7.3</td>
-            <td className="tableMaterias text-success">Aprobado</td>
-          </tr>
-          <tr>
-            <td className="tableMaterias">Matemáticas</td>
-            <td className="tableMaterias">2°B</td>
-            <td className="tableMaterias">Prof. Juan Perez</td>
-            <td className="tableMaterias">6</td>
-            <td className="tableMaterias">9</td>
-            <td className="tableMaterias">7</td>
-            <td className="tableMaterias">7.3</td>
-            <td className="tableMaterias text-success">Aprobado</td>
-          </tr>
-          <tr>
-            <td className="tableMaterias">Matemáticas</td>
-            <td className="tableMaterias">2°B</td>
-            <td className="tableMaterias">Prof. Juan Perez</td>
-            <td className="tableMaterias">6</td>
-            <td className="tableMaterias">9</td>
-            <td className="tableMaterias">7</td>
-            <td className="tableMaterias">7.3</td>
-            <td className="tableMaterias text-success">Aprobado</td>
-          </tr>
-          <tr>
-            <td className="tableMaterias">Matemáticas</td>
-            <td className="tableMaterias">2°B</td>
-            <td className="tableMaterias">Prof. Juan Perez</td>
-            <td className="tableMaterias">6</td>
-            <td className="tableMaterias">9</td>
-            <td className="tableMaterias">7</td>
-            <td className="tableMaterias">7.3</td>
-            <td className="tableMaterias text-success">Aprobado</td>
-          </tr>
-          <tr>
-            <td className="tableMaterias">Matemáticas</td>
-            <td className="tableMaterias">2°B</td>
-            <td className="tableMaterias">Prof. Juan Perez</td>
-            <td className="tableMaterias">6</td>
-            <td className="tableMaterias">9</td>
-            <td className="tableMaterias">7</td>
-            <td className="tableMaterias">7.3</td>
-            <td className="tableMaterias text-success">Aprobado</td>
-          </tr>
-          <tr>
-            <td className="tableMaterias">Matemáticas</td>
-            <td className="tableMaterias">2°B</td>
-            <td className="tableMaterias">Prof. Juan Perez</td>
-            <td className="tableMaterias">6</td>
-            <td className="tableMaterias">9</td>
-            <td className="tableMaterias">7</td>
-            <td className="tableMaterias">7.3</td>
-            <td className="tableMaterias text-success">Aprobado</td>
-          </tr>
-          <tr>
-            <td className="tableMaterias">Matemáticas</td>
-            <td className="tableMaterias">2°B</td>
-            <td className="tableMaterias">Prof. Juan Perez</td>
-            <td className="tableMaterias">6</td>
-            <td className="tableMaterias">9</td>
-            <td className="tableMaterias">7</td>
-            <td className="tableMaterias">7.3</td>
-            <td className="tableMaterias text-success">Aprobado</td>
-          </tr>
-          <tr>
-            <td className="tableMaterias">Matemáticas</td>
-            <td className="tableMaterias">2°B</td>
-            <td className="tableMaterias">Prof. Juan Perez</td>
-            <td className="tableMaterias">6</td>
-            <td className="tableMaterias">9</td>
-            <td className="tableMaterias">7</td>
-            <td className="tableMaterias">7.3</td>
-            <td className="tableMaterias text-success">Aprobado</td>
-          </tr>
-          <tr>
-            <td className="tableMaterias">Matemáticas</td>
-            <td className="tableMaterias">2°B</td>
-            <td className="tableMaterias">Prof. Juan Perez</td>
-            <td className="tableMaterias">6</td>
-            <td className="tableMaterias">9</td>
-            <td className="tableMaterias">7</td>
-            <td className="tableMaterias">7.3</td>
-            <td className="tableMaterias text-success">Aprobado</td>
-          </tr>
+          {materias.length > 0 ? (
+            materias.map((materia, index) => (
+              <tr key={index}>
+                <td className="tableMaterias">{materia.nombre}</td>
+                <td className="tableMaterias">{materia.anio}°</td>
+                <td className="tableMaterias">{materia.docente}Docente a cargo</td>
+                <td className="tableMaterias">{materia.trimestre1}</td>
+                <td className="tableMaterias">{materia.trimestre2}</td>
+                <td className="tableMaterias">{materia.trimestre3}</td>
+                <td className="tableMaterias">{materia.notaFinal}</td>
+                <td
+                  className={`tableMaterias ${
+                    materia.estado === "Aprobado"
+                      ? "text-success"
+                      : "text-danger"
+                  }`}
+                >
+                  {materia.estado}
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="8">No se encontraron materias.</td>
+            </tr>
+          )}
         </tbody>
       </Table>
+
       <section className="mt-4">
-        <h6 className="fs-5 text-primary-emphasis">Listado Materias previas de año anterior (en caso de tener)</h6>
+        <h6 className="fs-5 text-primary-emphasis">
+          Listado Materias previas de año anterior (en caso de tener)
+        </h6>
         <Table hover responsive className="my-3 rounded ">
-          <thead className="">
+          <thead>
             <tr>
               <th className="tableMaterias">Materia</th>
               <th className="tableMaterias">Año</th>
@@ -168,14 +92,7 @@ const Materias = () => {
               <th className="tableMaterias">Estado</th>
             </tr>
           </thead>
-          <tbody>
-            <tr>
-              <td className="tableMaterias">Quimica</td>
-              <td className="tableMaterias">1°B</td>
-              <td className="tableMaterias">Prof. Juan Perez</td>
-              <th className="text-warning tableMaterias">Previa</th>
-            </tr>
-          </tbody>
+          <tbody>{/* Similar logic for previas */}</tbody>
         </Table>
       </section>
     </Container>
