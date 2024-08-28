55d22f728712a153f70b7c35cba64702c4532510
Juanchi <toranzos628@gmail.com>
2024-08-28 20:38:11 -0300

    modificacion de las variables de entorno

--- src/stores/Asistencias-Store.jsx
+++ src/stores/Asistencias-Store.jsx
@@ -1,6 +1,11 @@
 import { create } from 'zustand';
 import axios from 'axios';
 
+const URL_ASISTENCIA = import.meta.env.VITE_API_ASISTENCIA;
+const URL_USUARIO = import.meta.env.VITE_API_USUARIO;
+const URL_MATERIA = import.meta.env.VITE_API_MATERIA;
+
+
 const useStore = create((set) => ({
   usuarios: [],
   materias: [],
@@ -8,21 +13,21 @@ const useStore = create((set) => ({
   historialAsistencias: [],
 
   fetchUsuarios: async () => {
-    const response = await axios.get('http://localhost:3000/usuarios');
+    const response = await axios.get(`${URL_USUARIO}`);
     set({ usuarios: response.data });
   },
 
   fetchMaterias: async () => {
-    const response = await axios.get('http://localhost:3000/materias');
+    const response = await axios.get(`${URL_MATERIA}`);
     set({ materias: response.data });
   },
 
   fetchAsistencias: async () => {
-    const response = await axios.get('http://localhost:3000/asistencias');
+    const response = await axios.get(`${URL_ASISTENCIA}`);
     set({ asistencias: response.data });
   },
    fetchHistorial: async () => {  // Nueva función para obtener el historial de asistencias
-    const response = await axios.get('http://localhost:3000/asistencias');
+    const response = await axios.get(`${URL_ASISTENCIA}`);
     set({ historialAsistencias: response.data });
   },
 
@@ -31,7 +36,7 @@ const useStore = create((set) => ({
     set((state) => ({
       asistencias: [...state.asistencias, nuevaAsistencia],
     }));
-    axios.post('http://localhost:3000/asistencias', nuevaAsistencia);
+    axios.post(`${URL_ASISTENCIA}`, nuevaAsistencia);
   },
 }));
 
